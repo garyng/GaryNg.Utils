8c7fdec772d3a719a1430bc430da29e067e8f369
garyng <garyng.zhongbo@gmail.com>
2020-09-20 21:33:33 +0800

    feat: add void

--- src/GaryNg.Utils.Void/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GaryNg.Utils.Void
-{
-	public class Class1
-	{
-	}
-}
---
+++ src/GaryNg.Utils.Void/VoidExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+
+namespace GaryNg.Utils.Void
+{
+	public static class VoidExtensions
+	{
+		public static IObservable<Void> ToVoid(this IObservable<System.Reactive.Unit> @this) =>
+			@this.Select(_ => Void.Value);
+
+		public static IObservable<Void> ToVoid<T>(this IObservable<T> @this) => @this.Select(_ => Void.Value);
+
+		public static Task<Void> ToVoid(this Task<System.Reactive.Unit> @this) => Task.FromResult(Void.Value);
+	}
+}

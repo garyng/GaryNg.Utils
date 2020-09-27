a9b28752011a98983dc57e04c3ff4149e234a6f4
garyng <garyng.zhongbo@gmail.com>
2020-09-27 18:13:09 +0800

    feat: enumerable extensions

---
+++ src/GaryNg.Utils.Enumerable/EnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GaryNg.Utils.Enumerable
+{
+	public static class EnumerableExtensions
+	{
+		public static bool Empty<T>(this IEnumerable<T> @this)
+		{
+			return !@this.Any();
+		}
+	}
+}

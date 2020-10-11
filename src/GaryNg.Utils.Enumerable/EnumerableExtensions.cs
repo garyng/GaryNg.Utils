3ef23dc5d09a45ca0a1a6ee0c5f762eb285120b1
garyng <garyng.zhongbo@gmail.com>
2020-10-11 13:09:53 +0800

    feat(enumerable): more null related extensions

--- src/GaryNg.Utils.Enumerable/EnumerableExtensions.cs
+++ src/GaryNg.Utils.Enumerable/EnumerableExtensions.cs
@@ -9,6 +9,29 @@ namespace GaryNg.Utils.Enumerable
 	{
 		public static bool Empty<T>(this IEnumerable<T> @this) => !@this.Any();
 
-		public static IEnumerable<T> NotNull<T>(this IEnumerable<T> @this) => @this.Where(x => x != null);
+		public static IEnumerable<T> NotNull<T>(this IEnumerable<T> @this)
+			=> @this.Where(x => x != null);
+
+		public static IEnumerable<T> NotNullBy<T, U>(this IEnumerable<T> @this, Func<T, U> select)
+			=> @this.Where(x => @select(x) != null);
+
+		public static IEnumerable<U> SelectNotNull<T, U>(this IEnumerable<T> @this, Func<T, U> select)
+			=> @this.Select(select).Where(x => x != null);
+
+		/// <summary>
+		/// Map source with <see cref="select"/>, remove nulls and flatten the resulting <see cref="IEnumerable{T}"/>
+		/// </summary>
+		public static IEnumerable<U> SelectManyNotNull<T, U>(this IEnumerable<T> @this, Func<T, IEnumerable<U>> select)
+			=> @this.Select(select).Where(x => x != null).SelectMany(x => x);
+		
+		/// <summary>
+		/// Map source with <see cref="select"/>, remove nulls, and flatten with <see cref="selectMany"/>.
+		/// </summary>
+		public static IEnumerable<V> SelectManyNotNull<T, U, V>(this IEnumerable<T> @this,
+			Func<T, U> select,
+			Func<U, IEnumerable<V>> selectMany)
+			=> @this.Select(select).Where(x => x != null).SelectMany(selectMany);
+
+	
 	}
 }
